9be009034c44a52efab2c092c8c64f2f7c47600b
Caden Keen <cadenkeen@gmail.com>
2025-01-25 09:13:35 -0500

    Update

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -29,11 +29,15 @@ export function ThemeToggle() {
   }
 
   return (
-    <Select value={theme} onValueChange={setTheme}>
+    <Select value={mounted ? theme : undefined} onValueChange={setTheme}>
       <SelectTrigger className="w-[120px] bg-[rgba(24,191,191,0.1)] border-[#18BFBF] text-[#00FFFF]">
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <SelectValue placeholder="Theme" />
+        {mounted && (
+          <>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </>
+        )}
+        <SelectValue placeholder={mounted ? "Theme" : ""} />
       </SelectTrigger>
       <SelectContent className="bg-[#0F2F2F] border-[#18BFBF]">
         {COLOR_SCHEMES.map((scheme) => (
